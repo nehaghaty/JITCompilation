312d94dd2fd69156edad0c1bbbf173ccd1bfbc8a
Neha Ghaty <neha.ghatysash@gmail.com>
2023-10-26 18:57:14 -0700

    fix errors

--- p2/scheduler.c
+++ p2/scheduler.c
@@ -7,23 +7,28 @@
  * scheduler.c
  */
 
+#undef _FORTIFY_SOURCE
+
 #include <unistd.h>
 #include <signal.h>
 #include <setjmp.h>
 #include "system.h"
 #include "scheduler.h"
-#undef _FORTIFY_SOURCE
-#define NDEBUG
-#define STACK_SIZE (1024 * 1024)
+
+
+/**
+ * Needs:
+ *   setjmp()
+ *   longjmp()
+ */
+
+/* research the above Needed API and design accordingly */
 
 /* define static state */
 static struct{
-    /* linked list of threads */
-    struct thread * head;
-    /* currently executing/running thread, to find running thread in O(1) time */
-    struct thread* thread;
-    /* mark a common place for scheduler */
-    jmp_buf ctx;
+    struct thread *head; /* linked list of threads */
+    struct thread *curr_thread; /* currently executing thread */
+    jmp_buf ctx; /* scheduler context */
 } state;
 
 /* Thread Data Structure */
@@ -41,34 +46,55 @@ struct thread {
 	} stack;
     scheduler_fnc_t fnc;
     void *arg;
-    struct thread* link; /* linked ptr to next thread */
+    struct thread* next;
 }thread;
 
-/* struct thread* thread_create(){
-     struct thread* my_thread = malloc(sizeof(thread));
-     my_thread->stack.memory_ = malloc(1048576);
-     size_t p_s = page_size();
-     my_thread->stack.memory = memory_align(p_s, my_thread->stack.memory_);
-     my_thread->link = NULL;
-     my_thread->status = STATUS_;
- }*/
+struct thread* thread_create(scheduler_fnc_t fnc, void *arg){
+    struct thread* new_thread = (struct thread *)malloc(sizeof(struct thread));
+    size_t page_sz = page_size();
+    new_thread->stack.memory_ = (void*)malloc(4*page_sz);
+    if (!new_thread->stack.memory_) {
+        perror("malloc");
+        return NULL; /* error in memory allocation */
+    }
+    new_thread->stack.memory = memory_align(new_thread->stack.memory_, page_sz);
+    new_thread->status = STATUS_;
+    new_thread->fnc = fnc;
+    new_thread->arg = arg;
+    new_thread->next = NULL;
+    return new_thread;
+ }
 
 struct thread* thread_candidate(void){
+    struct thread* current_ref;
+    struct thread* current;
+    if(state.curr_thread == NULL){
+        current = state.head;
+        current_ref = state.head;
+    }
+    else{
+        current = state.curr_thread->next;
+        current_ref = state.curr_thread->next;
+    }
     /*traverse list, find valid states: state_, state_sleeping
     return next thread
     return null if nothing runnable: terminating condition, else return thread object */
-    struct thread *current = state.head;
-    while (current != NULL) {
+    do{
+        if(current==NULL){
+            current = state.head;
+        }
         if (current->status == STATUS_ || current->status == STATUS_SLEEPING) {
             return current;
         }
-        current = current->link;
-    }
+        current = current->next;
+ 
+    }while (current != current_ref);
     return NULL;
 
 }
 
 void schedule(void){
+
     /*
         create thread object = thread candidate
             if thread candidate is null return 
@@ -82,28 +108,30 @@ void schedule(void){
     */
 
         struct thread *thread = thread_candidate();
-        if (!thread) {
+        if (thread == NULL) {
             /* all threads completed execution */
             return;
         } else {
-                state.thread = thread;
-                if (thread->status == STATUS_) {
-                    uint64_t rsp = (uint64_t)thread->stack.memory + STACK_SIZE; 
-                    /* Simulated assembly code */ 
+                size_t page_sz = page_size();
+                uint64_t rsp;
+                state.curr_thread = thread;
+                if (state.curr_thread->status == STATUS_) {
+                    state.curr_thread->status = STATUS_RUNNING;
+                    rsp = (uint64_t)thread->stack.memory + 3*page_sz;
                     __asm__ volatile("mov %[rs], %%rsp \n" : [rs] "+r"(rsp)::);
-                    thread->status = STATUS_RUNNING;
-                    thread->fnc(thread->arg);
-                } else {
-                    thread->status = STATUS_RUNNING;
-                    longjmp(thread->ctx, 1);
+                    (*state.curr_thread->fnc)(state.curr_thread->arg);
+                    state.curr_thread->status = STATUS_TERMINATED;
+                    longjmp(state.ctx, 1);
+
+                } else if(state.curr_thread->status == STATUS_SLEEPING) {
+                    state.curr_thread->status = STATUS_RUNNING;
+                    longjmp(state.curr_thread->ctx, 1); 
                 }
     }
 }
 
-
-
-/*dstroy call at the end */
 void destroy(void){
+
     /*
     traverse the linkedlist
     find head
@@ -115,59 +143,28 @@ void destroy(void){
     while (current != NULL) {
         /* put in function */
         struct thread* temp = current;
-        current = current->link;
+        current = current->next;
         free(temp->stack.memory_);
         free(temp);
     }
     state.head = NULL;
-    state.thread = NULL;
+    state.curr_thread = NULL;
 }
 
 int scheduler_create(scheduler_fnc_t fnc, void *arg ){
-    
-    /*
-    gets the page_size
-    memory aligns the page
-    allocate thread
-    init
-        fnc array
-        allocate stack
-    link it state head
-    thread* new_thread = thread_create();
-    new_thread->link = state.head;
-    state.head = new_thread;
-    return 0;
-    */
-    size_t page_sz = page_size();
-    void *stack_memory = malloc(page_sz + STACK_SIZE);
-    struct thread *new_thread = (struct thread *)malloc(sizeof(struct thread));
-
-    if (!stack_memory) {
-        perror("malloc");
-        return -1; /* error in memory allocation */
+    struct thread *new_thread;
+    new_thread = thread_create(fnc, arg);
+    if(!new_thread){
+        return -1;
     }
-    if (!new_thread) {
-        free(stack_memory);
-        return -1; /* error in memory allocation*/
-    }
-    new_thread->status = STATUS_;
-    new_thread->stack.memory_ = stack_memory;
-    new_thread->stack.memory = memory_align(stack_memory, page_size());
-    new_thread->fnc = fnc;
-    new_thread->arg = arg;
-    new_thread->link = state.head;
+    new_thread->next = state.head;
     state.head = new_thread;
     return 0; /* success */
-
-
 }
 
 void scheduler_execute(void){
-    /* checkpoint -> state ctx setjmp(state.ctx)
-     schedule() <-- majority of time will be spent here
-    destroy() */
-    setjmp(state.ctx);
-    schedule(); /* majority of time will be spent here */
+    setjmp(state.ctx); /* checkpoint */
+    schedule(); /* majority of the time will be spent here */
     destroy();
 }
 
@@ -180,17 +177,15 @@ void scheduler_yield(void){
     longjmp(state.ctx) <-- go back to old state 
     */
    /* checkpoint-> currently running thread */
-   if(!setjmp(state.thread->ctx)){
-        state.thread->status = STATUS_SLEEPING;
+   int val = setjmp(state.curr_thread->ctx);
+   if(val==0){
+        state.curr_thread->status = STATUS_SLEEPING;
         longjmp(state.ctx, 1);
+   }
+   else{
+    return;
    }
 
 }
 
-/**
- * Needs:
- *   setjmp()
- *   longjmp()
- */
 
-/* research the above Needed API and design accordingly */
